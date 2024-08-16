6fab8a972be91893a5bdbdde32aa50eb515cfd2d
daniejl04 <danielestebanjimenezlopez@gmail.com>
2024-08-16 14:32:18 -0500

    created services create user

--- src/module/users/users.service.ts
+++ src/module/users/users.service.ts
@@ -1,12 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './entities/user.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>){}
+async create(createUserDto: CreateUserDto) {
+    const existingUser = await this.userModel
+      .findOne({ email: createUserDto.email })
+      .exec();
+      if (existingUser) {
+        throw new HttpException(
+          `User with email ${createUserDto.email} already exists`,
+          HttpStatus.BAD_REQUEST,
+        );
   }
+}
 
   findAll() {
     return `This action returns all users`;
