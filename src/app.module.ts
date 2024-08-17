2ec6d359a97cb139e2e74e713c57b23adacd3e96
Daniel Jimenez <98568163+daniejl04@users.noreply.github.com>
2024-08-16 19:08:41 -0500

    Revert "Feat/mudule user"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './module/users/users.module';
-import { PersistenceModule } from './persistance/persistance';
-import { ConfigModule } from '@nestjs/config';
-import dbConfig from './persistance/persistance/db-config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      load: [dbConfig],
-      isGlobal: true,
-    }),
-    PersistenceModule,
-    UsersModule,
-  ],
-  controllers: [],
-  providers: [],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-
 export class AppModule {}
